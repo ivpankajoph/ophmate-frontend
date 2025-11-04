f2b74d0b9ab806e41d12ceaf4ebab0312af587c6
ivpankajoph <pankaj@onlinepromotionhouse.com>
2025-11-04 13:10:00 +0530

    Asd

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
-  remotePatterns: [
-    {
-      protocol: "http",
-      hostname: "localhost",
-      port: "8000",
-      pathname: "/**", // ✅ allow all paths
-    },
-    {
-      protocol: "https",
-      hostname: "flagcdn.com",
-      pathname: "/**",
-    },
-  ],
-},
-
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**", // allow all local images
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "phpstack-1522038-5968955.cloudwaysapps.com",
+        pathname: "/**", // ✅ allow all paths under this host
+      },
+    ],
+  },
 };
 
 export default nextConfig;
