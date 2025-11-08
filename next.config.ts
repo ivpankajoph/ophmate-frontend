338a3db82261da5c6f240b062c2f9985931233e6
ivpankajoph <pankaj@onlinepromotionhouse.com>
2025-11-08 17:27:37 +0530

    asd

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
-        pathname: "/**", // allow all local images
+        pathname: "/**",
       },
       {
         protocol: "https",
