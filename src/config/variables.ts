7d1fb403dd6a22a5413bf9ca1640b4b69011e221
ivpankajoph <pankaj@onlinepromotionhouse.com>
2025-11-08 12:15:23 +0530

    asd

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,14 +1,14 @@
-// export const NEXT_PUBLIC_API_URL="https://localhost:8000/api"
-// export const NEXT_PUBLIC_API_URL_BANNERS="https://localhost:8000"
+// export const NEXT_PUBLIC_API_URL="http://localhost:8080/api"
+// export const NEXT_PUBLIC_API_URL_BANNERS="http://localhost:8080"
 
 
 
-export const NEXT_PUBLIC_API_URL="https://phpstack-1522038-5968955.cloudwaysapps.com/api"
-export const NEXT_PUBLIC_API_URL_BANNERS="https://phpstack-1522038-5968955.cloudwaysapps.com"
+// export const NEXT_PUBLIC_API_URL="https://phpstack-1522038-5968955.cloudwaysapps.com/api"
+// export const NEXT_PUBLIC_API_URL_BANNERS="https://phpstack-1522038-5968955.cloudwaysapps.com"
 
 
-// export const NEXT_PUBLIC_API_URL="https://ophmate-backend-579008086831.us-central1.run.app/api"
-// export const NEXT_PUBLIC_API_URL_BANNERS="https://ophmate-backend-579008086831.us-central1.run.app"
+export const NEXT_PUBLIC_API_URL="https://ophmate-backend-579008086831.us-central1.run.app/api"
+export const NEXT_PUBLIC_API_URL_BANNERS="https://ophmate-backend-579008086831.us-central1.run.app"
 
 
 // export const NEXT_PUBLIC_API_URL="https://ophmate-backend.onrender.com/api"
