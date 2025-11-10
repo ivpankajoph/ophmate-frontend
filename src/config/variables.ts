8a8ddf3233a2491f89b97390a91c6c8ea4a40d65
ivpankajoph <pankaj@onlinepromotionhouse.com>
2025-11-10 17:59:15 +0530

    s

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,5 +1,5 @@
-// export const NEXT_PUBLIC_API_URL="http://localhost:8080/api"
-// export const NEXT_PUBLIC_API_URL_BANNERS="http://localhost:8080"
+export const NEXT_PUBLIC_API_URL="http://localhost:8080/api"
+export const NEXT_PUBLIC_API_URL_BANNERS="http://localhost:8080"
 
 
 
@@ -7,8 +7,8 @@
 // export const NEXT_PUBLIC_API_URL_BANNERS="https://phpstack-1522038-5968955.cloudwaysapps.com"
 
 
-export const NEXT_PUBLIC_API_URL="https://ophmate-backend-579008086831.us-central1.run.app/api"
-export const NEXT_PUBLIC_API_URL_BANNERS="https://ophmate-backend-579008086831.us-central1.run.app"
+// export const NEXT_PUBLIC_API_URL="https://ophmate-backend-579008086831.us-central1.run.app/api"
+// export const NEXT_PUBLIC_API_URL_BANNERS="https://ophmate-backend-579008086831.us-central1.run.app"
 
 
 // export const NEXT_PUBLIC_API_URL="https://ophmate-backend.onrender.com/api"
